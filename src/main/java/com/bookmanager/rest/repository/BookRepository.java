e786c78568e662ecaa9579d5ba653932d005ebe9
Yuriy <ukim3817@gmail.com>
2025-06-14 16:40:30 +0500

    refactor: add methods to BookRepository interface

--- src/main/java/com/bookmanager/rest/repository/BookRepository.java
+++ src/main/java/com/bookmanager/rest/repository/BookRepository.java
@@ -1,7 +1,14 @@
 package com.bookmanager.rest.repository;
 
+import com.bookmanager.rest.entity.Author;
 import com.bookmanager.rest.entity.Book;
+import com.bookmanager.rest.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book,Long> {
+    List<Book> findByAuthor(Author author);
+    List<Book> findByGenre(Genre genre);
+    Book findByTitle(String title);
 }
---
+++ src/main/java/com/bookmanager/rest/service/book/BookService.java
@@ -0,0 +1,4 @@
+package com.bookmanager.rest.service.book;
+
+public class BookService {
+}
